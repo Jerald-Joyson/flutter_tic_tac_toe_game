db42325f1160588534a9a354eb2907728a130fb4
Jerald-Joyson <jeraldjoyson.inc@gmail.com>
2023-05-28 13:11:30 +0530

    first commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = const Color(0xFF201E20);
+  static Color secondaryColor = const Color(0xFFDDC3A5);
+  static Color accentColor = const Color(0xFFE0A96D);
+}
